f35cf7a718a0b22d0759675c221e06e75bbc0bef
“DonatelloTHM” <donat.pllana@outlook.com>
2020-06-17 14:20:57 -0400

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,35 @@
 require_relative './part_1_solution.rb'
-
+require "pry"
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-end
+
+  coupons.each do |coupon|
+    cart.each do |item|
+      if(item[:count]>=coupon[:num] && coupon[:item]==item[:item])
+        item_with_coupon={}
+        item_with_coupon[:item]=item[:item]+" W/COUPON"
+        item_with_coupon[:price]= coupon[:cost] / coupon[:num]
+        item_with_coupon[:clearance] = item[:clearance]
+        item_with_coupon[:count]=coupon[:num]
+
+        cart.push(item_with_coupon)
+        item[:count] -= coupon[:num]
+        
+        end
+      end
+  end
+    cart
+  end
+
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart.each do |item|
+    if item[:clearance]==true
+      item[:price]=item[:price] - item[:price] * 0.20
+    end
+end
 end
 
 def checkout(cart, coupons)
@@ -22,4 +42,15 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidated= consolidate_cart(cart)
+  couponized =apply_coupons(consolidated,coupons)
+  finalized =apply_clearance(couponized)
+  sum=0
+  finalized.each do |item|
+    sum+=(item[:price]*item[:count])
+  end
+  if(sum>100)
+    sum=sum-sum*0.10
+  end
+  sum
 end
